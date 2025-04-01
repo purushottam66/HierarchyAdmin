331988848edaed9f09f8b60c16cdb2c5da79cb3d
purushottam66 <128211134+purushottam66@users.noreply.github.com>
2025-04-01 12:09:12 +0530

    update code
    update code

--- application/config/config.php
+++ application/config/config.php
@@ -381,7 +381,7 @@ $config['sess_expiration'] = 60;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = true;
+$config['sess_regenerate_destroy'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
